a4f0e81e0bfa93cfc3013b539b5dcd96abe6444e
nguyenvanduocit <nguyenvanduocit@gmail.com>
2015-04-17 09:35:14 +0700

    add updateer

---
+++ update.php
@@ -0,0 +1,22 @@
+<?php 
+
+require_once dirname(__FILE__) . '/inc/inc.update.php';
+if (!class_exists('ET_Mailing_Update')){
+
+	class ET_Mailing_Update extends CE_Plugin_Updater{
+		const VERSION = '1.0.0';
+
+		// setting up updater
+		public function __construct(){
+			$this->product_slug 	= plugin_basename( dirname(__FILE__) . '/et_mailing.php' );
+			$this->slug 			= 'et_mailing';
+			$this->license_key 		= get_option('et_license_key', '');
+			$this->current_version 	= self::VERSION;
+			$this->update_path 		= 'http://www.enginethemes.com/forums/?do=product-update&product=et_mailing&type=plugin';
+
+			parent::__construct();
+		}
+	}
+}
+
+new ET_Mailing_Update();
